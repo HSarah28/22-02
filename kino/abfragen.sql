ced0d84ad06e12d39ff1ea187be2c9891dc62c44
HSarah28 <123622140+HSarah28@users.noreply.github.com>
2024-02-29 08:07:06 +0000

    29.02

--- kino/abfragen.sql
+++ kino/abfragen.sql
@@ -1,2 +1,30 @@
 SELECT * FROM Film;
-SELECT * FROM Regisseur;
+
+SELECT Regisseur, SUM(Länge) AS Gesamtlänge
+FROM Filme
+GROUP BY Regisseur;
+
+SELECT Veröffentlichungsjahr, COUNT(*) AS Anzahl_Filme
+FROM Filme
+GROUP BY Veröffentlichungsjahr;
+
+
+SELECT 
+  CONCAT(LEFT(Veröffentlichungsjahr, 3), '0s') AS Jahrzehnt,
+  AVG(Länge) AS Durchschnittslänge
+FROM Filme
+GROUP BY LEFT(Veröffentlichungsjahr, 3);
+
+SELECT 
+  Veröffentlichungsjahr,
+  MAX(Länge) AS Maximale_Länge,
+  MIN(Länge) AS Minimale_Länge
+FROM Filme
+GROUP BY Veröffentlichungsjahr;
+
+
+SELECT RegisseurID, COUNT(FilmID) AS AnzahlFilme
+
+FROM Film
+
+GROUP BY RegisseurID;
