fa0f6442a3e5bd48f07f0a2389379ca8d4097e87
HSarah28 <123622140+HSarah28@users.noreply.github.com>
2024-03-21 08:07:10 +0000

    21.03

--- kino/abfragen.sql
+++ kino/abfragen.sql
@@ -28,3 +28,38 @@ SELECT RegisseurID, COUNT(FilmID) AS AnzahlFilme
 FROM Film
 
 GROUP BY RegisseurID;
+
+
+--Challenge 1--
+SELECT  DISTINCT Jahr FROM Film
+ORDER BY Jahr
+SELECT MIN (Jahr) AS ältestes_Erscheinungsjahr FROM Film;
+ 
+--Challenge 2--
+SELECT DISTINCT Länge FROM Film
+ORDER BY Länge DESC
+LIMIT 1;
+
+--Challenge 3--
+SELECT COUNT(*) AS Anzahl_Filme_Ridley_Scott
+FROM Film
+INNER JOIN Regisseur ON Film.RegisseurID = Regisseur.RegisseurID
+WHERE Regisseur.Nachname = 'Scott';
+ 
+--Challenge 4--
+SELECT ROUND(AVG(Länge), 0) AS Durchschnittliche_Länge
+FROM Film;
+ 
+ 
+--Challenge 5--
+SELECT Regisseur.Name, COUNT(Film.ID) AS Anzahl_Filme
+FROM Regisseur
+LEFT JOIN Film ON Regisseur.ID = Film.Regisseur_ID
+GROUP BY Regisseur.Name;
+ 
+--Challenge 6--
+SELECT Erscheinungsjahr, COUNT(*) AS Anzahl_Filme
+FROM Film
+GROUP BY Erscheinungsjahr
+ORDER BY Anzahl_Filme DESC
+LIMIT 1;
